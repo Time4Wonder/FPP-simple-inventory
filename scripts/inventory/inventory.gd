3b83b1662bb52923532bb5458a2f082053fc8683
t4w <lorenz.burgard@gmail.com>
2025-12-13 10:25:32 +0100

    grundlegendes inventar system

--- scripts/inventory/inventory.gd
+++ scripts/inventory/inventory.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const ITEM_PICKUP = preload("uid://cnt808fskfi32")
+
 @export var max_slots: int = 4
 var items: Array[ItemData] = []
 var current_slot_index = 0
@@ -8,9 +10,6 @@ signal inventory_updated # Signal to the UI
 
 func _ready() -> void:
 	items.resize(max_slots)
-	print(items[0])
-	print(items)
-
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("drop_item"):
@@ -33,24 +32,40 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func add_item(item: ItemData):
-	for i in range(items.size()): 
-		if items[i] == null:
-			items[i] = item
-			inventory_updated.emit()
-			break
-	print(items)
+	if items[current_slot_index] == null:
+		items[current_slot_index] = item
+	else:
+		for i in range(items.size()): 
+			if items[i] == null:
+				items[i] = item
+				inventory_updated.emit()
+				break
 	
 func drop_item_current_item():
 	if items[current_slot_index] != null:
+
+		
+		
+		var drop: RigidBody3D = ITEM_PICKUP.instantiate()
+		drop.item_data = items[current_slot_index]
+		
+		# adds the drop on the current treescene
+		get_tree().current_scene.add_child(drop)
+		#setting position
+		var camera = get_viewport().get_camera_3d()
+		var drop_position = camera.global_position + (camera.global_transform.basis.z * -1.5)
+		drop.global_position = drop_position
+		# Adding physics 
+		var throw_direciton =  -camera.global_transform.basis.z
+		var force = 5.0
+		drop.apply_impulse(throw_direciton * force + Vector3(0,2,0))
+		
 		items[current_slot_index] = null
 		inventory_updated.emit()
-		
-		print(items)
 	
 func switchItem():
 	current_slot_index += 1
 	inventory_updated.emit()
-	print(items)
 	
 func is_full() -> bool:
 	for i in items:
